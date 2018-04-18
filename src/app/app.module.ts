85f320c45bdfae1ad4d1927dac8f01c9e1b1c5c7
acs17 <aleixcs17@gmail.com>
2018-04-18 20:23:05 +0200

    Intent httpClient get per a la URL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule} from '@angular/forms';
 import { TinyMceModule, tinymceDefaultSettings } from 'angular-tinymce';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -21,6 +22,7 @@ import { WorkComponent } from './work/work.component';
     AppRoutingModule,
     FormsModule,
     TinyMceModule.forRoot(tinymceDefaultSettings()),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-work',
@@ -8,11 +12,21 @@ import { Component, OnInit } from '@angular/core';
 export class WorkComponent implements OnInit {
 
   filename: string;
+  url: string = 'Load an url to replace this sample text with its content...';
+  auxiliar: Observable<any>;
   content: string = 'Load a file to replace this sample text with its content...';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+  }
+
+  getUrl(url){
+    this.auxiliar = this.http.get<any>('url');
+  }
+
+  saveUrl(){
+    alert(this.auxiliar);
   }
 
   loadFile(input) {
